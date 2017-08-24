8a6da616ab5b9c33012c1f7d1edfb847311fd5e8
zach <zweishar@isovera.com>
2017-08-24 06:55:52 -0500

    Update basic typographic settings

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,6 +1,11 @@
 import Typography from 'typography'
 import Alton from 'typography-theme-alton'
 
+Alton.baseFontSize = '20px'
+Alton.baseLineHeight = 1.5
+Alton.scaleRatio = 2
+Alton.includeNormalize = true
+
 const typography = new Typography(Alton)
 
 // Hot reload typography in development.
